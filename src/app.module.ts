60690862b16d07579c017ef500bd6406e8f2c825
Dawar Shamim <DawarShamim1@gmail.com>
2024-03-23 18:27:03 +0500

    added jwt authentication and login route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirstController } from './first/first.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, FirstController],
   providers: [AppService],
 })
