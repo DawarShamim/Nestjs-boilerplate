f2432920f939b1992dc5dd31e9673c9162456f76
Dawar Shamim <DawarShamim1@gmail.com>
2024-03-23 18:05:47 +0500

    adde swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('nest project Starter Template')
+    .setDescription('nest project Starter Template')
+    .setVersion('1.0')
+    // .addTag('started')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(new LoggerMiddleware().use);
   await app.listen(3000);
 }
